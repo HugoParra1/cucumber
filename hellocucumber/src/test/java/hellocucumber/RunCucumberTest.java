a8aaf13f49fd7b786aeea588b7ccd82ec74d7d89
HugoParra1 <hugo.pargon@gmail.com>
2024-05-21 19:08:06 +0200

    reportes

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -1,15 +1,25 @@
 package hellocucumber;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;       
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("hellocucumber")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = { "hellocucumber" }, features = {"src/test/resources/hellocucumber"}, plugin = { "pretty", "json:target/surefire-reports/cucumber.json", "html:target/surefire-reports/cucumber.html" })    
 public class RunCucumberTest {
+
 }
+// import org.junit.platform.suite.api.ConfigurationParameter;
+// import org.junit.platform.suite.api.IncludeEngines;
+// import org.junit.platform.suite.api.SelectClasspathResource;
+// import org.junit.platform.suite.api.Suite;
+
+// import static
+// io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+// @Suite
+// @IncludeEngines("cucumber")
+// @SelectClasspathResource("hellocucumber")
+// @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+// public class RunCucumberTest {
+// }
