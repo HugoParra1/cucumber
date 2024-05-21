f683a8399a9bc12cbd3126288b2163f445c4321d
HugoParra1 <hugo.pargon@gmail.com>
2024-05-21 17:43:39 +0200

    Ejercicio clase Friday y Saturday

--- hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -7,6 +7,21 @@ import io.cucumber.java.en.When;
 // import org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * InnerStepDefinitions
+ */
+class IsItFriday {
+    static String isFriday(String day) {
+        return day.equals("Friday") ? "Yes" : "No";
+
+    };
+};
+class IsItDay {
+    static String isDay(String day, String expected) {
+        return day.equals(expected) ? "Yes" : "No";
+    };
+};
+
 public class StepDefinitions {
     private String today;
     private String answer;
@@ -17,19 +32,34 @@ public class StepDefinitions {
         // throw new io.cucumber.java.PendingException();
     }
 
-    @When("I ask if it's friday")
-    public void i_ask_if_it_s_friday() {
-        if (today.equals("friday")) {
-            answer = "Yes";
-        } else {
-            answer = "No";
-        }
+    @Given("it is Friday")
+    public void it_is_friday() {
+        today = "Friday";
         // throw new io.cucumber.java.PendingException();
     }
 
-    @Then("I should get told i'ts {string}")
+    @Given("today is {string}")
+    public void today_is(String string) {
+        today = string;
+        // throw new io.cucumber.java.PendingException();
+    };
+
+    @When("I ask if it's {string}")
+    public void i_ask_if_it_s(String string) {
+        answer = IsItDay.isDay(today, string);
+        // throw new io.cucumber.java.PendingException();
+    }   
+    @When("I ask if it's friday")
+    public void i_ask_if_it_s_friday() {
+        answer = IsItFriday.isFriday(today);
+        // throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should get told its {string}")
     public void i_should_get_told_i_ts(String string) {
         assertEquals(string, answer);
         // throw new io.cucumber.java.PendingException();
     }
+
 }
+
--- hellocucumber/target/test-classes/hellocucumber/its_friday.feature
+++ hellocucumber/target/test-classes/hellocucumber/its_friday.feature
@@ -1,7 +1,34 @@
 Feature: Friday_test
-    everybody wants to know if it's friday
+  everybody wants to know if it's friday
 
-    Scenario: Sunday is not friday
-        Given it is Sunday
-        When I ask if it's friday
-        Then I should get told i'ts "No"
+  @Friday
+  Scenario Outline: Today is or is not Friday
+    Given today is "<day>"
+    When I ask if it's "<expected>"
+    Then I should get told its "<answer>"
+    @Friday1
+    Examples:
+      | day    | expected | answer |
+      | Friday | Friday   | Yes    |
+      | Sunday | Friday   | No     |
+    @Friday2
+    Examples:
+      | day           | expected | answer |
+      | Saturday      | Friday   | No     |
+      | Anything else | Friday   | No     |
+
+  @Saturday
+  Scenario Outline: Today is or is not Saturday
+    Given today is "<day>"
+    When I ask if it's "<expected>"
+    Then I should get told its "<answer>"
+    @Saturday1
+    Examples:
+      | day    | expected | answer |
+      | Friday | Saturday | No     |
+      | Sunday | Saturday | No     |
+    @Saturday2
+    Examples:
+      | day           | expected | answer |
+      | Saturday      | Saturday | Yes    |
+      | Anything else | Saturday | No     |
